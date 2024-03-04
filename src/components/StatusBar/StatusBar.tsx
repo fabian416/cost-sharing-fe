2cd6d06f1d3131715bd475ce88f48ef9dde06680
fabian <fabian@MacBook-Air-de-fabian.local>
2024-03-03 23:18:11 -0300

    new structure

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -3,10 +3,20 @@ import WalletConnectImg from "../../assets/walletConnect/walletConnectButton.png
 import useStatusBar from "./useStatusBar";
 import MaticIcon from "../../assets/walletConnect/polygonMaticIcon.png";
 import ProfilePic from "../../assets/walletConnect/userProfile.png";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function StatusBar() {
+  const navigate = useNavigate();
   const { openWalletConnectModal, address, isConnected } = useStatusBar();
   const reducedAddress = `${address?.slice(0, 10)}...`;
+
+  useEffect(() => {
+    if (isConnected) {
+      navigate('/dashboard'); // Cambia esto por la ruta que quieras después de la conexión
+    }
+  }, [isConnected, navigate]);
+
 
   return (
     <section className={styles.statusbarContainer}>
