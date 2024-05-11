31599369342b1b8f94df389ae37d13784c4af0d1
fabian416 <diazfabian99@icloud.com>
2024-05-10 23:50:04 -0300

    finish connect wallet iteration

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../../components/SideBar/SideBar';
+import ConnectButton from '../../components/ConnectButton/ConnectButton';
 import { Outlet } from 'react-router-dom';
 import styles from './Dashboard.module.css';
+import { useWeb3ModalAccount } from '@web3modal/ethers5/react';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const { isConnected } = useWeb3ModalAccount();  // Obtiene el estado de conexión desde useWeb3Modal
+
+  useEffect(() => {
+      if (!isConnected) {  // Si isConnected es false, navega al login
+          navigate('/');
+      }
+  }, [isConnected, navigate]);  // Dependencias del efecto
+
   return (
-    <div className={styles.dashboard}>
+    <div className={styles.dashboardContainer}>
       <Sidebar />
-      <div className={styles.content}>
+      <div className={styles.mainContent}>
+        <div className={styles.topBar}>
+          <ConnectButton />
+        </div>
         <Outlet /> {/* Aquí se renderizarán los componentes GeneralPanel, GroupDetails, etc. */}
       </div>
     </div>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,44 +1,26 @@
-import React, { useState } from 'react';
-import RegistrationModal from './RegistrationModal';
-import WalletButton from '../../components/WalletButton/WalletButton';
+import React, { useEffect } from 'react';
+import ConnectButton from '../../components/ConnectButton/ConnectButton';
+import { useWeb3ModalAccount } from '@web3modal/ethers5/react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 
-// Home.tsx
-
 const Home = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isRegistered, setIsRegistered] = useState(localStorage.getItem('isRegistered') === 'true');
     const navigate = useNavigate();
+    const { isConnected } = useWeb3ModalAccount();  // Obtiene el estado de conexión desde useWeb3Modal
 
-    const handleRegistrationSuccess = (nickname: string, email: string) => {
-        console.log("Registrando:", nickname, email); // Verifica que los datos lleguen aquí
-        localStorage.setItem('isRegistered', 'true');
-        localStorage.setItem('nickname', nickname);
-        localStorage.setItem('email', email);
-        setIsRegistered(true);
-        setIsModalOpen(false);
-        navigate('/dashboard');
-    };
-    const redirectToDashboard = () => {
-        navigate('/dashboard');
-    };
+    useEffect(() => {
+        if (isConnected) {  // Si isConnected es true, navega al dashboard
+            navigate('./dashboard')
+        }
+    }, [isConnected, navigate]);  // Dependencias del efecto
+
     return (
         <div className={styles.homeContainer}>
             <h1 className={styles.title}>Welcome to Squary</h1>
             <p className={styles.description}>Share expenses and settle up easy with Crypto.</p>
             <div className={styles.walletButtonContainer}>
-                {isRegistered ? (
-                    <WalletButton onClick={redirectToDashboard} />
-                ) : (
-                    <button onClick={() => setIsModalOpen(true)}>Register / Connect Wallet</button>
-                )}
+                <ConnectButton/>
             </div>
-            <RegistrationModal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                onRegister={handleRegistrationSuccess}
-            />
         </div>
     );
 };
