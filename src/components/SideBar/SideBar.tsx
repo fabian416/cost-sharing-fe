7d14441623fd06f9c869c9bc2611c2cf137ddb1e
fabian416 <diazfabian99@icloud.com>
2024-05-15 11:24:38 -0300

    sidebar buttons and styles, last stable changes

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import styles from './SideBar.module.css'; // Importa correctamente el módulo CSS
+import styles from './SideBar.module.css'; // Asegúrate de tener la ruta correcta
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
-      <h1>Logo</h1>
-      <ul>
-        <li>Inicio</li>
-        <li>Perfil</li>
-        <li>Ajustes</li>
-        <li>Salir</li>
-      </ul>
+      <h1 className={styles.logo}>Squary</h1>
+      <div className={styles.section}>
+        <div className={styles.sectionTitle}>General Panel</div>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.sectionTitle}>Groups</div>
+        <div className={styles.subSection}>+</div>
+        <ul>
+          <li>Group 1</li>
+          <li>Group 2</li>
+          <li>Group 3</li>
+          <li>Group 4</li>
+          <li>Group 5</li>
+          <li>Group 6</li>
+          <li>Group 7</li>
+          <li>Group 8</li>
+          {/* Agrega más grupos según sea necesario */}
+        </ul>
+      </div>
+      <div className={styles.section}>
+        <div className={styles.sectionTitle}>Friends</div>
+        <div className={styles.subSection}>+</div>
+      </div>
     </div>
   );
-};
+}
 
 export default Sidebar;
+
+
+
+
+
