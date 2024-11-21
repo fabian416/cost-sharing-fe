bc10388d3ff8192fd379d90de72d407d15326e2a
fabiandiaz416 <diazfabian99@icloud.com>
2024-11-20 21:18:18 -0300

    delete tag Options

--- src/components/GroupOptions/GroupOptions.tsx
+++ src/components/GroupOptions/GroupOptions.tsx
@@ -210,7 +210,7 @@ const GroupOptions: React.FC<GroupOptionsProps> = ({ groupId, groupName }) => {
 
   return (
     <div className={styles.groupOptions}>
-      <h1 className={styles.title}>{groupName} Options</h1>
+      <h1 className={styles.title}>{groupName}</h1>
       <div className={styles.buttonsContainer}>
         <button
           className={`${styles.button} ${styles.addExpense}`}
