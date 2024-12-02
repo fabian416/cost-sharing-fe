6fc544c953ee291d43e1254d661cb6b4a14f8e90
fabiandiaz416 <diazfabian99@icloud.com>
2024-12-02 14:47:16 -0300

    delete unused import

--- src/components/GroupOptions/ExpenseModal/ExpenseModal.tsx
+++ src/components/GroupOptions/ExpenseModal/ExpenseModal.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import styles from './ExpenseModal.module.css';
-import { useUser } from '../../../utils/UserContext';
 import { useENS } from '../../../hooks/useEns';
 
 interface ExpenseModalProps {
